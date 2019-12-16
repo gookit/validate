41874e0fe7949c41b518f2f9889e1c98b71babfc
Inhere <inhere@users.noreply.github.com>
2019-12-16 20:24:46 +0800

    fix ci error for Max check

--- validators_test.go
+++ validators_test.go
@@ -250,9 +250,10 @@ func TestMax(t *testing.T) {
 	is.True(Max(3, 4))
 	is.True(Max(3, 3))
 	is.True(Max(int64(3), 3))
+	// nil will as 0
+	is.True(Max(nil, 3))
 
 	// fail
-	is.False(Max(nil, 3))
 	is.False(Max("str", 3))
 	is.False(Max(3, 2))
 	is.False(Max(int64(3), 2))
